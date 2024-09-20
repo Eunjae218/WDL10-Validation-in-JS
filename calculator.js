59c79459ae3ae7db75558be94c63e85cb9e676e9
Eunjae218 <eunjae.yu5981@gmail.com>
2024-09-20 18:34:08 +0700

    added a1 and a2 and half a3

---
+++ calculator.js
@@ -0,0 +1,9 @@
+//clear the calculator display
+function clearScreen(){
+    document.getElementById("result").value = " "
+}
+
+//appends the clicked button value to the display
+function setScreenValue(value){
+    
+}
---
+++ script.js
@@ -0,0 +1,24 @@
+//A1
+function validateForm() {
+    let formItems = document.forms["myform"]["fName"].value;
+    if (formItems == ""){
+        alert("NO NAME GIVEN PLEASE FILL THE FORM ")
+        return false
+    }
+}
+
+//A2
+let numInput = document.getElementById("numy")
+let result = document.getElementById("result")
+
+function submit() {
+    if (!numInput.checkValidity()){
+        result.innerHTML = numInput.validationMessage
+    }
+    if (numInput > 300){
+        result.innerHTML = numInput.validationMessage
+    }
+    if (numInput < 100){
+        result.innerHTML = numInput.validateMessage
+    }
+}
