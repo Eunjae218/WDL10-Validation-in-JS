f4400a37d8a32e7a667dc506d02c7ac5ef985114
Eunjae218 <eunjae.yu5981@gmail.com>
2024-09-27 18:15:28 +0700

    completed the calculator

--- calculator.js
+++ calculator.js
@@ -1,9 +1,26 @@
+const result = document.getElementById("result")
+
 //clear the calculator display
 function clearScreen(){
-    document.getElementById("result").value = " "
+    result.value = "";
 }
 
 //appends the clicked button value to the display
 function setScreenValue(value){
-    
+    result.value += value;
+}
+
+//calculate
+function calculateResult(){
+    let expression = result.value.trim();
+    if (expression === ""){
+        result.value = "Enter an expression";
+        return
+    }
+    try{
+        result.value = eval(expression)
+    }
+    catch(err){
+        result.value = "invalid expression";
+    }
 }
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //A1
 function validateForm() {
     let formItems = document.forms["myform"]["fName"].value;
-    if (formItems == ""){
+    if (formItems == "") {
         alert("NO NAME GIVEN PLEASE FILL THE FORM ")
         return false
     }
@@ -12,13 +12,13 @@ let numInput = document.getElementById("numy")
 let result = document.getElementById("result")
 
 function submit() {
-    if (!numInput.checkValidity()){
+    if (!numInput.checkValidity()) {
         result.innerHTML = numInput.validationMessage
     }
-    if (numInput > 300){
+    if (numInput > 300) {
         result.innerHTML = numInput.validationMessage
     }
-    if (numInput < 100){
+    if (numInput < 100) {
         result.innerHTML = numInput.validateMessage
     }
 }
